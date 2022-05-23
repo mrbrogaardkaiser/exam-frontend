00774520730bf9b4ef16c6b8fb95d0d32e727a70
Lars <larsbrogaard86@gmail.com>
2022-05-23 14:07:28 +0200

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+import{ renderTemplate, setActive, showPage} from "./utility.js"
+
+
+
+function renderNavItems(evt){
+    const element = evt.target
+    setActive(element)
+    const id = element.id
+    renderTemplate(id)
+    switch (id){
+        case "home": {
+            break
+        }
+        case "unit":{
+            break
+        }
+        case "dummy":{
+            break
+        }
+       
+    }
+}
+
+document.getElementById("navbar").onclick= renderNavItems;
+showPage("home")
