6001b4293f91e74627c4e64d9ac6ecc0d166ba4c
Lars <larsbrogaard86@gmail.com>
2022-05-30 15:56:50 +0200

    delete

--- js-for-pages/manage-riders.js
+++ js-for-pages/manage-riders.js
@@ -4,10 +4,14 @@ import { handleHttpErrors } from "../fetchUtils.js"
 const URL = rootApi+"/riders"
 
 export function setupRiderHandlers(){
-
     document.getElementById("btn-add-rider").onclick=addRider;
+    getAllRiders();
+    document.getElementById("btn-delete-rider").onclick=deleteRider;
  
 }
+
+
+
 
 function addRider(){
     const addPostForm = document.querySelector(".add-post-form")
@@ -36,5 +40,60 @@ function addRider(){
         const dataArr = [];
         dataArr.push(data);
     })
+})
+}
+
+function getAllRiders(){
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const rows = data.map(r=>
+                `
+        <tr>
+            <td>${r.id}</td>
+            <td>${r.name}</td>
+            <td>${r.nation}</td>
+            <td>${r.dateOfBirth}</td>
+            <td>${r.timeInSeconds}</td>
+            <td>${r.climberPoints}</td>
+            <td>${r.sprintPoint}</td>
+            
+            
+        </tr>    
+        `).join("\n")
+            document.getElementById("tbl-id").innerHTML=rows;
+        })
+
+        .catch(err => console.log("Error: " + err))
+        .finally(err => console.log("Done"))
+}
+
+
+
+
+function editRider(){
+    
+}
+
+function deleteRider(){
+    const addPostForm = document.querySelector(".delete-post-form")
+    const id = document.getElementById("delete-id").value
+
+    addPostForm.addEventListener("click", (e)=> {
+        e.preventDefault();
+
+        console.log(id)
+
+
+    fetch(URL+"/"+id,
+    {
+        method: "DELETE",
+        
+    })
+        
+        .catch(err => console.log("Error: " + err))
+        .finally(err => console.log("Done"))
+        
 })
 }
